7622496ae5308d1401e58d1a96957a6feda62175
Lustytail <qkrehdgns91@gmail.com>
2024-04-11 16:46:16 +0900

    edit

--- init.js
+++ init.js
@@ -10,16 +10,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     console.log(req.query);
-    res.send("get API");
+    res.send("get API\n" + JSON.stringify(req.query) + "\n");
 });
 
 app.post('/', (req, res) => {
     console.log(req.body);
-    res.send("post API");
+    res.send("post API\n" + JSON.stringify(req.body) + "\n");
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), "대기중");
+    console.log(app.get('port'), "Dummy Server is running...");
 });
 
 
